797e920860dab773d13fd6d82e5ec14467025c80
VietBac <81665443+vietbac0967@users.noreply.github.com>
2024-03-29 16:13:10 +0700

    finish call API 1b

--- PracticeService/src/main/java/com/example/practiceservice/controllers/PracticeController.java
+++ PracticeService/src/main/java/com/example/practiceservice/controllers/PracticeController.java
@@ -1,18 +1,41 @@
 package com.example.practiceservice.controllers;
 
 import com.example.practiceservice.models.Subject;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.HttpServerErrorException;
+import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
 
 @RestController
 @RequestMapping("/api/practice")
 public class PracticeController {
+
+    @PostMapping
+    public ResponseEntity<Subject> createSubjectFormPractice(@RequestBody Subject subject) {
+        RestTemplate restTemplate = new RestTemplate();
+        String url = "http://localhost:8088/api/subject";
+
+        try {
+            ResponseEntity<Subject> responseEntity = restTemplate.postForEntity(url, subject, Subject.class);
+            return ResponseEntity.status(responseEntity.getStatusCode()).body(responseEntity.getBody());
+        } catch (HttpClientErrorException | HttpServerErrorException ex) {
+            // Handle client or server errors
+            return ResponseEntity.status(ex.getRawStatusCode()).body(null);
+        } catch (RestClientException ex) {
+            // Handle other REST client exceptions
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
+        }
+    }
+
     @GetMapping("/{id}")
-    public Subject getSubjectFormPractice(@PathVariable("id") Long id){
+    public Subject getSubjectFormPractice(@PathVariable("id") Long id) {
         RestTemplate restTemplate = new RestTemplate();
         String url = "http://localhost:8088/api/subject/" + id;
-        return  restTemplate.getForObject(url,Subject.class);
+        return restTemplate.getForObject(url, Subject.class);
     }
 
 
